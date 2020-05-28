d88876ffab8bedc4e3e5662fb3583623008f6935
tjr735 <tjr735@cs.bham.ac.uk>
2020-05-28 15:03:57 +0100

    Added ticks on selecting / taking photo so you know when you have loaded images Added logging

--- app/src/main/java/com/tjr/wordsearchsolver/ui/load/LoadFragment.java
+++ app/src/main/java/com/tjr/wordsearchsolver/ui/load/LoadFragment.java
@@ -1,6 +1,7 @@
 package com.tjr.wordsearchsolver.ui.load;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -12,10 +13,14 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.FileProvider;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import com.tjr.wordsearchsolver.R;
 import com.tjr.wordsearchsolver.data.DataStore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,21 +33,28 @@ import static com.tjr.wordsearchsolver.data.RequestCodes.TAKE_WORDS_PHOTO;
 
 public class LoadFragment extends Fragment implements View.OnClickListener {
 
+    private final Logger logger = LoggerFactory.getLogger(LoadFragment.class);
+
+    private Button loadWordsearchCameraButton;
+    private Button loadWordsearchPhotoButton;
+    private Button loadWordsCameraButton;
+    private Button loadWordsPhotoButton;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_load, container, false);
 
         // Set button listeners
-        Button loadWordsearchCameraButton = root.findViewById(R.id.button_load_wordsearch_camera);
+        loadWordsearchCameraButton = root.findViewById(R.id.button_load_wordsearch_camera);
         loadWordsearchCameraButton.setOnClickListener(this);
 
-        Button loadWordsearchPhotoButton = root.findViewById(R.id.button_load_wordsearch_photo);
+        loadWordsearchPhotoButton = root.findViewById(R.id.button_load_wordsearch_photo);
         loadWordsearchPhotoButton.setOnClickListener(this);
 
-        Button loadWordsCameraButton = root.findViewById(R.id.button_load_words_camera);
+        loadWordsCameraButton = root.findViewById(R.id.button_load_words_camera);
         loadWordsCameraButton.setOnClickListener(this);
 
-        Button loadWordsPhotoButton = root.findViewById(R.id.button_load_words_photo);
+        loadWordsPhotoButton = root.findViewById(R.id.button_load_words_photo);
         loadWordsPhotoButton.setOnClickListener(this);
 
         return root;
@@ -71,10 +83,23 @@ public class LoadFragment extends Fragment implements View.OnClickListener {
         if (resultCode == RESULT_OK) {
             if (requestCode == LOAD_WORDSEARCH_PHOTO || requestCode == LOAD_WORDS_PHOTO) {
                 Uri selectedImageURI = data.getData();
-                if (requestCode == LOAD_WORDSEARCH_PHOTO)
+                if (requestCode == LOAD_WORDSEARCH_PHOTO && checkBitmapIsntEmpty(selectedImageURI)) {
                     DataStore.getDataStore().setWordsearchImage(selectedImageURI);
-                else
+                    loadWordsearchPhotoButton.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_tick, null), null, null, null);
+                    loadWordsearchCameraButton.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_camera, null), null, null, null);
+                } else {
                     DataStore.getDataStore().setSearchWordsImage(selectedImageURI);
+                    loadWordsPhotoButton.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_tick, null), null, null, null);
+                    loadWordsCameraButton.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_camera, null), null, null, null);
+                }
+            } else if (requestCode == TAKE_WORDSEARCH_PHOTO || requestCode == TAKE_WORDS_PHOTO) {
+                if (requestCode == TAKE_WORDSEARCH_PHOTO && checkBitmapIsntEmpty(DataStore.getDataStore().getWordsearchImage())) {
+                    loadWordsearchCameraButton.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_tick, null), null, null, null);
+                    loadWordsearchPhotoButton.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_photo, null), null, null, null);
+                } else if (checkBitmapIsntEmpty(DataStore.getDataStore().getSearchWordsImage())) {
+                    loadWordsCameraButton.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_tick, null), null, null, null);
+                    loadWordsPhotoButton.setCompoundDrawablesWithIntrinsicBounds(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_photo, null), null, null, null);
+                }
             }
         }
     }
@@ -87,18 +112,18 @@ public class LoadFragment extends Fragment implements View.OnClickListener {
     private void takePicture(int requestCode) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Make sure there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+        if (takePictureIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
             // Create the File where the photo should go
             File photoFile = null;
             try {
                 photoFile = createImageFile(requestCode);
-            } catch (IOException ex) {
-
+            } catch (IOException e) {
+                logger.error("Failed to create error", e);
             }
 
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(getContext(), "com.example.android.fileprovider", photoFile);
+                Uri photoURI = FileProvider.getUriForFile(requireActivity().getApplicationContext(), "com.example.android.fileprovider", photoFile);
 
                 if (requestCode == TAKE_WORDSEARCH_PHOTO)
                     DataStore.getDataStore().setWordsearchImage(photoURI);
@@ -119,7 +144,20 @@ public class LoadFragment extends Fragment implements View.OnClickListener {
         else if (requestCode == TAKE_WORDS_PHOTO)
             imageFileName += "Words_";
 
-        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         return File.createTempFile(imageFileName, ".jpg", storageDir);
+    }
+
+    private boolean checkBitmapIsntEmpty(Uri uri) {
+        Bitmap bitmap = null;
+        Bitmap emptyBitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(requireActivity().getContentResolver(), uri);
+            emptyBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
+        } catch (IOException e) {
+            logger.error("Error checking if bitmaps are empty", e);
+        }
+
+        return bitmap != null && !bitmap.sameAs(emptyBitmap);
     }
 }
